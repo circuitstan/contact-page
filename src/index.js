be9a40d6508149c8f76033ab16e30ce724e9bfb3
Stenver Suurkütt <stenver.ss@gmail.com>
2022-10-22 22:30:10 +0300

    add linkedIn

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ const contactInfo = {
     firstName: "Stenver",
     lastName: "Suurkütt",
     email: "hello@stenver.dev",
+    linkedin: ["stenver-suurkütt", "https://www.linkedin.com/in/stenver-suurk%C3%BCtt/"],
     github: ["circuitstan", "https://github.com/circuitstan"]
 }
 
 let keys = Object.keys(contactInfo)
 
 function displayInfo() {
-    document.querySelector('.open').textContent = "{"
+    document.querySelector('.open').textContent = "contactInfo = {"
 
     document.querySelector('.first-name').textContent = keys[0] + ": " + contactInfo.firstName
     document.querySelector('.last-name').textContent = keys[1] + ": " + contactInfo.lastName
@@ -22,8 +23,15 @@ function displayInfo() {
     emailLink.textContent = contactInfo.email
     email.appendChild(emailLink)
 
+    let linkedin = document.querySelector('.linkedin')
+    linkedin.textContent = keys[3] + ": "
+    let linkedinLink = document.createElement('a')
+    linkedinLink.href = contactInfo.linkedin[1]
+    linkedinLink.textContent = contactInfo.linkedin[0]
+    linkedin.appendChild(linkedinLink)
+
     let github = document.querySelector('.github')
-    github.textContent = keys[3] + ": "
+    github.textContent = keys[4] + ": "
     let githubLink = document.createElement('a')
     githubLink.href = contactInfo.github[1]
     githubLink.textContent = contactInfo.github[0]
